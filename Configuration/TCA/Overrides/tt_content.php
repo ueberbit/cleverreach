6afc4025cb1494ac345d1c60bdb8a1cf80fae517
Georg Großberger <g.grossberger@supseven.at>
2022-07-21 15:06:18 +0200

    [BUGFIX] Fix locallang collision with other EXT:cleverreach
    Also fixes a wrong ll reference

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'Cleverreach',
     'Pi1',
-    'LLL:EXT:cleverreach/Resources/Private/Language/locallang.xlf:plugin.cleverreach_pi1.title'
+    'LLL:EXT:cleverreach/Resources/Private/Language/shared.xlf:plugin.cleverreach_pi1.title'
 );
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['cleverreach_pi1'] = 'pages, recursive';
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'Cleverreach',
     'Pi2',
-    'LLL:EXT:cleverreach/Resources/Private/Language/locallang.xlf:plugin.cleverreach_pi2.title'
+    'LLL:EXT:cleverreach/Resources/Private/Language/shared.xlf:plugin.cleverreach_pi2.title'
 );
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['cleverreach_pi2'] = 'pages, recursive';
