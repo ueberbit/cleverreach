249774459f4d1d1d9b27f681a866f81351549daf
Georg Großberger <g.grossberger@supseven.at>
2023-09-19 14:56:07 +0200

    [TASK] Use CType instead of list_type for plugin

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -2,16 +2,39 @@
 
 declare(strict_types=1);
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'Cleverreach',
-    'Pi1',
-    'LLL:EXT:cleverreach/Resources/Private/Language/shared.xlf:plugin.cleverreach_pi1.title'
-);
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['cleverreach_pi1'] = 'pages, recursive';
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'Cleverreach',
-    'Pi2',
-    'LLL:EXT:cleverreach/Resources/Private/Language/shared.xlf:plugin.cleverreach_pi2.title'
-);
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['cleverreach_pi2'] = 'pages, recursive';
+(static function (): void {
+    $plugins = ['Optin', 'Optout'];
+
+    foreach ($plugins as $plugin) {
+        $key = 'cleverreach_' . strtolower($plugin);
+
+        ExtensionUtility::registerPlugin(
+            'Cleverreach',
+            $plugin,
+            'LLL:EXT:cleverreach/Resources/Private/Language/shared.xlf:plugin.' . $key . '.title',
+            'cleverreach',
+            'Newsletter'
+        );
+
+        $GLOBALS['TCA']['tt_content']['types'][$key]['showitem'] = '
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+                --palette--;;general,
+                --palette--;;headers,
+            --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+                --palette--;;frames,
+                --palette--;;appearanceLinks,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+                --palette--;;language,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+                --palette--;;hidden,
+                --palette--;;access,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+                categories,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+                rowDescription,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+        ';
+    }
+})();
--- ext_localconf.php
+++ ext_localconf.php
@@ -2,24 +2,26 @@
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
     'Cleverreach',
-    'Pi1',
+    'Optin',
     [
         \Supseven\Cleverreach\Controller\NewsletterController::class => 'optinForm, optinSubmit',
     ],
     [
         \Supseven\Cleverreach\Controller\NewsletterController::class => 'optinForm, optinSubmit',
-    ]
+    ],
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
 );
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
     'Cleverreach',
-    'Pi2',
+    'Optout',
     [
         \Supseven\Cleverreach\Controller\NewsletterController::class => 'optoutForm, optoutSubmit',
     ],
     [
         \Supseven\Cleverreach\Controller\NewsletterController::class => 'optoutForm, optoutSubmit',
-    ]
+    ],
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('@import \'EXT:cleverreach/Configuration/TsConfig/NewElementWizard.tsconfig\';');
